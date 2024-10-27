2b88cbd1b25b5d161a9292a98c482dc024cb3c1b
Alexandr Galaida <evaAxelrod@yandex.ru>
2024-10-27 18:21:07 +0500

    Create 278. First Bad Version.cs

---
+++ Problems/278. First Bad Version.cs
@@ -0,0 +1,19 @@
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        var l = 0;
+        var r = n;
+
+        while (r >= l){
+            var mid = l + (r-l)/2;
+
+            if (IsBadVersion(mid)){
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+}
