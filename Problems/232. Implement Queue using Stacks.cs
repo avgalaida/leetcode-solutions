acec3b462f7cd9f6d7068db45b85ca5cb0fe1e64
Alexandr Galaida <evaAxelrod@yandex.ru>
2024-10-29 11:09:46 +0500

    Update 232. Implement Queue using Stacks.cs

--- Problems/232. Implement Queue using Stacks.cs
+++ Problems/232. Implement Queue using Stacks.cs
@@ -1,34 +1,46 @@
-public class MyQueue {
+public class MyQueue
+{
     Stack<int> st1, st2;
 
-    public MyQueue() {
+    public MyQueue()
+    {
         st1 = new Stack<int>();
         st2 = new Stack<int>();
     }
-    
-    public void Push(int x) {
+
+    public void Push(int x)
+    {
         st1.Push(x);
     }
-    
-    public int Pop() {
-        if (st2.Count == 0) {
-            while (st1.Count > 0) {
+
+    public int Pop()
+    {
+        if (st2.Count == 0)
+        {
+            while (st1.Count > 0)
+            {
                 st2.Push(st1.Pop());
             }
         }
+
         return st2.Pop();
     }
-    
-    public int Peek() {
-        if (st2.Count == 0) {
-            while (st1.Count > 0) {
+
+    public int Peek()
+    {
+        if (st2.Count == 0)
+        {
+            while (st1.Count > 0)
+            {
                 st2.Push(st1.Pop());
             }
         }
+
         return st2.Peek();
     }
-    
-    public bool Empty() {
+
+    public bool Empty()
+    {
         return st1.Count == 0 && st2.Count == 0;
     }
 }
